08bd5b3f983b7a3b6117d3d4f1063fa8302142eb
deadfry42 <pooface442@gmail.com>
2025-12-17 21:12:46 +0000

    receive and respond to DiscordLoggingBridged

--- src/main/java/uk/co/nikodem/dFProxyPlugin/Discord/Utils/PluginConnectedServer.java
+++ src/main/java/uk/co/nikodem/dFProxyPlugin/Discord/Utils/PluginConnectedServer.java
@@ -1,8 +1,28 @@
 package uk.co.nikodem.dFProxyPlugin.Discord.Utils;
 
+import com.velocitypowered.api.proxy.server.RegisteredServer;
+import uk.co.nikodem.dFProxyPlugin.Config.Config;
+import uk.co.nikodem.dFProxyPlugin.DFProxyPlugin;
+
 import java.util.ArrayList;
 import java.util.List;
 
+import static uk.co.nikodem.dFProxyPlugin.Discord.Utils.BridgedChannelsHelper.getBridgedChannels;
+
 public class PluginConnectedServer {
     public static List<String> serverNamesConnected = new ArrayList<>();
+
+    public static boolean registerServer(RegisteredServer server) {
+        if (!DFProxyPlugin.config.discord_bot.isEnabled()) return false;
+        String name = server.getServerInfo().getName();
+
+        List<Config.DiscordBot.BridgedChannel> bridgedChannels = getBridgedChannels(name);
+        if (bridgedChannels.isEmpty()) return false;
+
+        if (!serverNamesConnected.contains(name)) {
+            serverNamesConnected.add(name);
+        }
+
+        return true;
+    }
 }
--- src/main/java/uk/co/nikodem/dFProxyPlugin/Messaging/Messages/DiscordLoggingBridged.java
+++ src/main/java/uk/co/nikodem/dFProxyPlugin/Messaging/Messages/DiscordLoggingBridged.java
@@ -2,11 +2,24 @@ package uk.co.nikodem.dFProxyPlugin.Messaging.Messages;
 
 import com.google.common.io.ByteArrayDataInput;
 import com.velocitypowered.api.event.connection.PluginMessageEvent;
+import com.velocitypowered.api.proxy.Player;
+import com.velocitypowered.api.proxy.ServerConnection;
+import com.velocitypowered.api.proxy.server.RegisteredServer;
+import uk.co.nikodem.dFProxyPlugin.Discord.Utils.PluginConnectedServer;
 import uk.co.nikodem.dFProxyPlugin.Messaging.DFPluginMessageHandler;
+
+import static uk.co.nikodem.dFProxyPlugin.Messaging.PluginMessageListener.IDENTIFIER;
 
 public class DiscordLoggingBridged implements DFPluginMessageHandler {
     @Override
     public void run(PluginMessageEvent event, ByteArrayDataInput in) {
+        if (event.getSource() instanceof ServerConnection serverConnection) {
+            Player plr = serverConnection.getPlayer();
+            RegisteredServer server = serverConnection.getServer();
 
+            byte[] msg = createMessage("DiscordLoggingBridged", convertBoolToString(PluginConnectedServer.registerServer(server)));
+
+            sendPluginMessageToBackendUsingPlayer(plr, IDENTIFIER, msg);
+        }
     }
 }
